5b348b86b24d3a0136607fdc16c4d656f73901d5
mqcute <banhmisocute@gmail.com>
2026-06-17 18:32:24 -0500

    added killzone, camera limits, left world boundary, and bottom world kill area thing

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,7 +4,8 @@ extends CharacterBody2D
 const SPEED = 350.0
 const JUMP_VELOCITY = -600.0
 
-var ORIGINAL_SCALE = $Sprite2D.scale
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var ORIGINAL_SCALE = sprite_2d.scale
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -28,11 +29,11 @@ func _physics_process(delta: float) -> void:
 #animations
 func _process(_delta: float) -> void:
 	if Input.get_axis("ui_left", "ui_right") and is_on_floor(): # walking
-		$Sprite2D.rotation_degrees = 10 if sin(Time.get_ticks_msec() * 2 * get_process_delta_time())/3 > 0 else -10
-		$Sprite2D.scale = Vector2(ORIGINAL_SCALE)
+		sprite_2d.rotation_degrees = 10 if sin(Time.get_ticks_msec() * 2 * get_process_delta_time())/3 > 0 else -10
+		sprite_2d.scale = Vector2(ORIGINAL_SCALE)
 	elif !is_on_floor(): # air
-		$Sprite2D.rotation = 0
-		$Sprite2D.scale = Vector2(ORIGINAL_SCALE.x,ORIGINAL_SCALE.y * 1.2)
+		sprite_2d.rotation = 0
+		sprite_2d.scale = Vector2(ORIGINAL_SCALE.x,ORIGINAL_SCALE.y * 1.2)
 	else: # idle
-		$Sprite2D.rotation = 0
-		$Sprite2D.scale = Vector2(ORIGINAL_SCALE)
+		sprite_2d.rotation = 0
+		sprite_2d.scale = Vector2(ORIGINAL_SCALE)
