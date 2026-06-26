50f6c3150885729e030267ad9302995c696dae8c
mqcute <banhmisocute@gmail.com>
2026-06-26 17:10:17 -0500

    added truecoin sounds

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,7 +1,16 @@
 extends Area2D
 
 @onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(_body: Node2D) -> void:
 	game_manager.add_point(1)
-	queue_free()
+	var i = randi_range(1, 3)
+	match i:
+		1:
+			animation_player.play("PickUp1")
+		2:
+			animation_player.play("PickUp2")
+		3:
+			animation_player.play("PickUp3")
+		
